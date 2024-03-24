44724e3d60088f57c4fe4b8bdc8aaf5b55bb784e
Tony Lee <lkhsoft@gmail.com>
2024-03-24 21:06:31 +0900

    17. TonyCard - SwiftUI

--- 17. TonyCard-SwiftUI/TonyCard-SwiftUI/ContentView.swift
+++ 17. TonyCard-SwiftUI/TonyCard-SwiftUI/ContentView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            Color(red: 0.09, green: 0.63, blue: 0.52)
+            Color(red: 0.70, green: 0.47, blue: 0.85)
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack {
                 Image("Tony")
@@ -26,12 +26,12 @@ struct ContentView: View {
                     .font(Font.custom("Pacifico-Regular", size: 40))
                     .bold()
                     .foregroundColor(.white)
-                Text("iOS Developer")
+                Text("Product Owner")
                     .font(.system(size: 25))
                     .foregroundColor(.white)
                 Divider()
                 InfoView(text: "+82 10 2667 4932", imageName: "phone.fill")
-                InfoView(text: "tony.lee@lineable.net", imageName: "envelope.fill")
+                InfoView(text: "tony.lee@toss.im", imageName: "envelope.fill")
             }
         }
     }
