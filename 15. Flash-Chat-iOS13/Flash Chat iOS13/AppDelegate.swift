13438f8846779de2b69eae9752d093d4283f4cf3
Tony Lee <lkhsoft@gmail.com>
2024-03-24 19:47:05 +0900

    15. Flash Chat - TableCell & ChatView

--- 15. Flash-Chat-iOS13/Flash Chat iOS13/AppDelegate.swift
+++ 15. Flash-Chat-iOS13/Flash Chat iOS13/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FirebaseCore
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     
         FirebaseApp.configure()
+        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.resignOnTouchOutside = true
+        
         return true
     }
 
